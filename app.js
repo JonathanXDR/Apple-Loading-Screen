0f532165ed138e84b8d4534e4e0f4683904ba442
Jonathan Russ <jonathan.russ@bluewin.ch>
2022-03-04 23:20:56 +0100

    change names

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 window.onload = function () {
-  const chimes = [
+  const chimePaths = [
     'media/Mac_mini_2020_Startup.m4a',
     'media/MacBook_Air_M1_2020_Startup.m4a',
     'media/Pre-T2-based_Mac_Startup.m4a',
     'media/T2-based_Mac_Startup.m4a',
   ];
 
-  // Get random chime
-  const item = chimes[Math.floor(Math.random() * chimes.length)];
+  // Get random path
+  const randomPath = chimePaths[Math.floor(Math.random() * chimePaths.length)];
 
   // set audio source
-  document.getElementById('audio').src = item;
+  const chime = document.getElementById('audioChime');
+  chime.src = randomPath;
+  // chime.play();
 };
